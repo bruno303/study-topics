51edf7465b3e1640d5ac1d518f230ac265fb316c
Bruno Oliveira <brunooliveira812@gmail.com>
2025-08-03 17:29:24 -0300

    improve logs and error handling of save-backup script

--- self_hosted/backup/save-backup.sh
+++ self_hosted/backup/save-backup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+DEBUG=0
+
+function logDebug () {
+  if [ $DEBUG -eq 1 ]; then
+    echo "$(date +"%Y-%m-%d %H:%M:%S") $1"
+  fi
+}
+
 set -a
 . .env
 set +a
@@ -9,15 +17,20 @@ DATE=$(date +%Y-%m-%d)
 ARCHIVE_NAME="./tmp/backup-$DATE.tar.gz"
 BACKUP_CONTEXT_DIR=$(pwd)
 
-echo "Temp file: $ARCHIVE_NAME"
-echo "Restic repo: $RESTIC_REPOSITORY"
-echo "Backup destination: $BACKUP_DEST_DIR"
+logDebug "Temp file: $ARCHIVE_NAME"
+logDebug "Restic repo: $RESTIC_REPOSITORY"
+logDebug "Backup destination: $BACKUP_DEST_DIR"
 
 # Create tar of current repo
 tar -czf "$ARCHIVE_NAME" -C "$RESTIC_REPOSITORY" .
 
+if [ ! -d "$BACKUP_DEST_DIR" ]; then
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Backup destination directory not found: $BACKUP_DEST_DIR" 1>&2
+  exit 1
+fi
+
 # Copy to flash drive
-rclone copy "$ARCHIVE_NAME" "$BACKUP_DEST_DIR" --progress
+rclone copy "$ARCHIVE_NAME" "$BACKUP_DEST_DIR" --progress > /dev/null 2>&1
 
 # Keep only the last backup on the flash drive
 cd "$BACKUP_DEST_DIR"
